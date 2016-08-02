24c4d368633f1ef6f95958c208f64bf76a242780
zhiyong zhu <zzycjcg@qq.com>
2016-08-02 22:36:21 +0800

    生成128位随机码

---
+++ shell/generate_128bit_random.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xxd -c64 -l 64 -p /dev/random
+# output
+#88a6a954bbd28a69526a69b8943d0b67e197fb0cabd6b619c8165e2eaa7aa2bd068f33d853d4dbbcfea8d0f12f8c547b7a85e9f0fce7a817954926aef39533c7
